bc460d3d3ce861d96caf853725473ce6e5e3ee4b
salgue441 <carlossalguero441@gmail.com>
2023-07-04 14:45:35 -0600

    Started working in the meshes and the corresponding shader data that will be passed throught the OpenGL pipeline

---
+++ src/render/meshes/base_mesh.py
@@ -0,0 +1,42 @@
+"""
+@file base_mesh.py
+@brief Contains the base mesh class. 
+@author Carlos Salguero
+@version 1.0
+@date 2023-07-04
+"""
+import numpy as np
+
+
+class BaseMesh:
+    def __init__(self) -> None:
+        """
+        Initializes the base mesh.
+        """
+        self.ctx = None
+        self.program = None
+        self.vbo_format = None
+        self.attributes: tuple[str, ...] = None
+        self.vao = None
+
+    def get_vertex_data(self) -> np.array:
+        ...
+
+    def get_vao(self) -> np.array:
+        """
+        Gets the vertex array object.
+        :return: The vertex array object
+        """
+        vertex_data = self.get_vertex_data()
+        vbo = self.ctx.buffer(vertex_data)
+        vao = self.ctx.vertex_array(
+            self.program, [(vbo, self.vbo_format, *self.attributes)], skip_errors=True
+        )
+
+        return vao
+
+    def render(self) -> None:
+        """
+        Renders the mesh.
+        """
+        self.vao.render()
---
+++ src/render/meshes/quad_mesh.py
@@ -0,0 +1,47 @@
+"""
+@file quad_mesh.py
+@brief Contains the quad mesh class. 
+@author Carlos Salguero
+@version 1.0
+@date 2023-07-04
+"""
+import numpy as np
+from core.constants.settings import BG_COLOR, WIN_RES
+from meshes.base_mesh import BaseMesh
+
+
+class QuadMesh(BaseMesh):
+    def __init__(self, app):
+        """
+        Initializes the quad mesh.
+        :param app: The main application
+        """
+        super().__init__()
+
+        self.app = app
+        self.ctx = app.ctx
+        self.program = app.shader_program.quad
+
+        # Format
+        self.vbo_format = "3f 3f"
+        self.attributes = ("in_position", "in_color")
+        self.vao = self.get_vao()
+
+    def get_vertex_data(self) -> np.array:
+        """
+        Gets the vertex data.
+        :return: The vertex data
+        """
+        vertices = [
+            (0.5, 0.5, 0.0),
+            (-0.5, 0.5, 0.0),
+            (-0.5, -0.5, 0.0),
+            (0.5, 0.5, 0.0),
+            (-0.5, -0.5, 0.0),
+            (0.5, -0.5, 0.0),
+        ]
+
+        colors = [(0, 1, 0), (1, 0, 0), (1, 1, 0), (0, 1, 0), (1, 1, 0), (0, 0, 1)]
+
+        vertex_data = np.hstack([vertices, colors], dtype=np.float32)
+        return vertex_data
